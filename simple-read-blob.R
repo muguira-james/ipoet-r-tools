fc0035c31e1143f8fe81b54fb3de5581e82d9db7
muguira-james <muguira.james@gmail.com>
2020-01-15 15:14:12 -0500

    add an example of how to get the files for both front-door and adv-front-door

---
+++ simple-read-blob.R
@@ -0,0 +1,86 @@
+
+library(AzureRMR)
+library(AzureStor)
+library(dplyr)
+
+setwd("~/code/ross/azure/ipoet-r-tools")
+
+account_name_env <- "r21ipoet"
+account_key <- "xkQAqkWSD5qzGdgPLCpuTvG6r0xpRs0vIGz+RZG3PE1wcTMrv16WORDSv1FWJwFRhDOyOT5bJ+ciAHbDYCJ08A=="
+
+account_name <- paste0("https://", account_name_env, ".blob.core.windows.net")
+# account_key <- paste0()
+Sys.setenv(STORAGE_CONN_STRING="DefaultEndpointsProtocol=https;AccountName=r21ipoet;AccountKey=xkQAqkWSD5qzGdgPLCpuTvG6r0xpRs0vIGz+RZG3PE1wcTMrv16WORDSv1FWJwFRhDOyOT5bJ+ciAHbDYCJ08A==;EndpointSuffix=core.windows.net")
+account_conn_string <- Sys.getenv("STORAGE_CONN_STRING")
+#
+print(c("account conn string = ", account_conn_string))
+print(c("storage account name = ", account_name, "storage key = ", account_key))
+#
+# this url points to a storage account I have setup in my resource group (goto portal.azure.com)
+# the key from the portal and points to the ipoetproxysa storage account
+#
+bl_endp_key <- storage_endpoint(account_name, key=account_key)
+#
+store <- list_storage_containers(bl_endp_key)
+#
+#
+queueName <- "front-door-ipoet"
+outQueueName <- "ipoet-1-queue"
+userid <- 1
+searchstring <- "http://www.google.com/search?q=US%20Sailing"
+ss <- paste(queueName, userid, searchstring, sep=" ")
+print(ss)
+system2("./add-string-queue.js", args=ss)
+#
+# to get the files that are queued
+num <- 0
+repeat {
+  num <- system2("./number-of-items-in-queue.js", args=outQueueName, stdout=TRUE)
+  print(c("num=",num))
+  if (num > 0) {
+    break
+  }
+  
+}
+
+#
+# to gather all the queued items - this just a single file
+dd <- system2("./drain-queue.js", args=outQueueName, stdout=TRUE)
+print(dd)
+# at this point dd is an array of {url, data, flag, userid} objects
+for (i in 1:length(dd)){
+  result = dd[i] %>% fromJSON()
+  print(c("url->", result$url, "blob File->", result$data))
+}
+#
+# outblobq is a blob contain I have setup in jam-que-rg
+cont <- storage_container(bl_endp_key, "advsearch-1-container")
+rr <- list_storage_files(cont)
+
+setwd("~/code/ross/azure/ipoet-r-tools/misc-files")
+
+# Get all the blobs
+
+# the params are: container, blob reference, dest dir
+for(j in 1:length(rr$Name)) {
+  fName <- paste0("test", j, ".json")
+  print(c(fName, rr$Name[j]))
+  storage_download(cont, rr$Name[j], fName)
+}
+
+
+# 
+#
+# experimental code - not needed to get blob contents
+#
+# tenant_id <- "48bf86e8-11a2-4b8a-8cb3-68d8be2227f3"
+# app_id <- "fb5f14f1-2417-476e-bfee-493efc5bf261"
+# 
+# password <- "97a4776f-921d-45a1-b9b6-91d431d98d1c"
+# subscription_id <- "d5351d73-3388-4c1f-aea1-cb78a3466165"
+# 
+# az <- az_rm$new(tenant=tenant_id,
+#                 app=app_id,
+#                 password=password)
+# 
+
